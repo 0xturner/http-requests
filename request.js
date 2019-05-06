5f68f1698d8bc81bc1397fd9d38c8be732e90678
Turner Schumann <turnerschumann@gmail.com>
2019-05-06 21:35:00 +0000

    Create Https client using request

---
+++ request.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+
+  .on('error', function (err) {
+    throw err;
+  })
+
+  .on('response', function (response) {
+    console.log('Downloading Image...')
+    console.log('Response Status Code: ' + response.statusCode);
+    console.log('Response Message: ' + response.statusMessage);
+    console.log('Response Content Type: ' + response.headers['content-type'])
+  })
+
+
+
+
+  .on('end', function (data) {
+    console.log('Downloading Complete.')
+  })
+
+  .pipe(fs.createWriteStream('./future.jpg'));
